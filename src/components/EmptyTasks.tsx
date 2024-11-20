114609498f39cc7819ae61d66eac73365ca7182d
bartashevich <shelest.dm@icloud.com>
2024-11-20 23:05:35 +0200

    first commit

--- src/components/EmptyTasks.tsx
+++
@@ -1,8 +0,0 @@
-export default function EmptyTasks() {
-    return (
-        <div className="animate-smoothAppearFromBottom w-full p-5 max-w-sm flex flex-col text-center gap-5 lg:gap-7 mx-auto">
-            <h2 className="text-lg lg:text-2xl font-bold">There is no task for you</h2>
-            <p className="text-[14px] lg:text-[16px] font-semibold">Forget about work, you can rest a little...for now :-)</p>
-        </div>
-    )
-};
